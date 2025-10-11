1d20e6a9e2c6acaac9b704b1eb54385488ca7f91
Miguel Chagas Maciel <miguelchagasmaciel@gmail.com>
2025-10-11 02:49:33 +0000

    primri

---
+++ macro.h
@@ -0,0 +1,11 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define print(fmt, ...) printf(fmt, __VA_ARGS__); printf("\n")
+
+#define repeat(qntd,codigo) for(int i = 0; i < qntd;i++){codigo}
+
+#endif
---
+++ main.c
@@ -0,0 +1,11 @@
+#include "macro.h"
+
+int main(){
+
+    int n = 5;
+
+    print("%d", n);
+    repeat(5, print("salve juninho"));
+    
+    return 0;
+}
